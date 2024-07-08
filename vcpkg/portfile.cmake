14ada30896f07210423ba682d42bba3e11b04482
syple000 <1519761174@qq.com>
2024-07-08 22:01:33 +0800

    更新vcpkg

--- vcpkg/portfile.cmake
+++ vcpkg/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@github.com:syple000/ab.git
-    REF ""
+    REF 0f86ca4a270ab5ce0ddebed21b93843acf7c96d3
 )
 
 vcpkg_cmake_configure(
