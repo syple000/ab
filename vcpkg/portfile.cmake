b2cbc9c256424fc8d36365bf574d033a57ae1b41
syple000 <1519761174@qq.com>
2024-07-09 08:45:41 +0800

    更新cmake安装路径

--- vcpkg/portfile.cmake
+++ vcpkg/portfile.cmake
@@ -3,15 +3,15 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@github.com:syple000/ab.git
-    REF edab99ed164a4da92de468b48cc71605f622450f
+    REF dd881e9ffdb5c6397dd9af07e813e9f76c7d85e0
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
 )
-vcpkg_cmake_config_fixup(PACKAGE_NAME "AE")
+vcpkg_cmake_config_fixup(PACKAGE_NAME "auto_engine")
 
 vcpkg_cmake_install()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+# file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
